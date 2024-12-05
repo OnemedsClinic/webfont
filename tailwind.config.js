c8c2011b83af3123b292e541aa62f77b2592f07c
OnemedsClinic <onemedsclinic@gmail.com>
2024-12-05 14:27:51 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'; // Use import instead of require
+
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}", // เพิ่ม .vue เพื่อรองรับ Vue ไฟล์
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [
-        require('daisyui'),
-    ],
-  };
-  
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui, // Use imported daisyui plugin here
+  ],
+};
